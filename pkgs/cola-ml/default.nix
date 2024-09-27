bb2c7d0159d64f2b56de1621b494c244d7747ddd
Stephen Huan <stephen.huan@cgdct.moe>
2024-09-27 07:44:21 -0400

    cola-ml: 0.0.5 -> 0.0.6

--- pkgs/cola-ml/default.nix
+++ pkgs/cola-ml/default.nix
@@ -1,6 +1,5 @@
 { lib
 , fetchFromGitHub
-, fetchpatch2
 , buildPythonPackage
 , setuptools
 , setuptools-scm
@@ -8,35 +7,23 @@
 , tqdm
 , cola-plum-dispatch
 , optree
-, pytreeclass
-, beartype
-, typing-extensions
-, pytest
 , pytestCheckHook
 , jax
 , jaxlib
 , torch
 }:
 
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "cola-ml";
-  version = "0.0.5";
+  version = "0.0.6";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "wilson-labs";
     repo = "cola";
-    rev = "9562ae1ff850ae99c96d5da441a5850184c66a1e";
-    hash = "sha256-swFJxCT0DkZpQnsSJXkmWZ4yEDh9k5MjA8xk5WxnCPU=";
+    rev = "v${version}";
+    hash = "sha256-w34M7VXrjJ/8Y45ki62aXwcOsNhiFs65YoIEYTXEhH4=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      name = "array-device.patch";
-      url = "https://github.com/wilson-labs/cola/pull/93.diff";
-      hash = "sha256-roo6lGrMTOtPGHRvK+fv4uVDmWpmNllIqtoIOn/JVSw=";
-    })
-  ];
 
   build-system = [
     setuptools
@@ -48,10 +35,6 @@ buildPythonPackage {
     tqdm
     cola-plum-dispatch
     optree
-    pytreeclass
-    beartype
-    typing-extensions
-    pytest
   ];
 
   nativeCheckInputs = [
@@ -62,19 +45,8 @@ buildPythonPackage {
   ];
 
   disabledTests = [
-    "test_get_lu_from_tridiagonal"
-    "test_vmappable_constructor"
-    "test_arnoldi_vjp"
-    "test_lanczos_vjp"
-    "test_unary"
-    "test_arnoldi_matrix_market"
-    "test_lanczos_matrix_market"
-  ];
-
-  disabledTestPaths = [
-    # tests that take a long time
-    "tests/linalg/test_inverse.py"
-    "tests/linalg/test_logdet.py"
+    # 'jaxlib.xla_extension.ArrayImpl' object has no attribute 'device'
+    "test_sparse"
   ];
 
   pythonImportsCheck = [
