7db6514f63227449d6902bcdd2a31082bce81700
Stephen Huan <stephen.huan@cgdct.moe>
2024-11-26 22:10:24 -0800

    fix(pkgs): remove orbax and tf-probability

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,26 +8,6 @@
     gpjax = final.callPackage ./gpjax { };
     mktestdocs = final.callPackage ./mktestdocs { };
     mugrade = final.callPackage ./mugrade { };
-    # see tensorflow-build in pkgs/top-level/python-packages.nix
-    orbax-checkpoint = prev.orbax-checkpoint.override {
-      protobuf = final.protobuf4.override {
-        protobuf = pkgs.protobuf_21.override {
-          abseil-cpp = pkgs.abseil-cpp_202301;
-        };
-      };
-    };
     pbbfmm3d = final.callPackage ./pbbfmm3d { };
-    tensorflow-probability = prev.tensorflow-probability.overridePythonAttrs
-      (previousAttrs: {
-        src = previousAttrs.src.overrideAttrs {
-          patches = [
-            (pkgs.fetchpatch2 {
-              name = "sort-kind.patch";
-              url = "https://github.com/tensorflow/probability/commit/3e652805bc2b57189c18df0228e3749f528fe21a.diff";
-              hash = "sha256-J6SjylsSSG+zazTTQAevyS/UXSYhSrkk3KPCjNEXNMQ=";
-            })
-          ];
-        };
-      });
   });
 }
