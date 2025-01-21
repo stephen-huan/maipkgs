1afe14933cba91a25845abdf932273b162a2eb78
Stephen Huan <stephen.huan@cgdct.moe>
2025-01-21 12:28:43 -0500

    jaxopt: remove

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,18 +8,6 @@
     cola-ml = final.callPackage ./cola-ml { };
     cola-plum-dispatch = final.callPackage ./cola-plum-dispatch { };
     gpjax = final.callPackage ./gpjax { };
-    jaxopt = prev.jaxopt.overridePythonAttrs (previousAttrs: {
-      patches = previousAttrs.patches or [ ] ++ [
-        (pkgs.fetchpatch2 {
-          name = "escape-sequences.patch";
-          url = "https://patch-diff.githubusercontent.com/raw/google/jaxopt/pull/591.diff";
-          hash = "sha256-u9a6H72lfsTR9lCjcocsEjPnv9UUbASNmRquL7EJeLA=";
-        })
-      ];
-      disabledTests = previousAttrs.disabledTests or [ ] ++ [
-        "test_logreg_with_intercept_manual_loop3"
-      ];
-    });
     jax-triton = final.callPackage ./jax-triton { };
     kernels = final.callPackage ./kernels { };
     mugrade = final.callPackage ./mugrade { };
