421a118f13132ccb0ce6a3e1e6988cef4301b098
Stephen Huan <stephen.huan@cgdct.moe>
2025-02-28 00:47:02 -0500

    fix(pkgs): add missing jax cuda libraries

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,20 @@
 
 let
   inherit (pkgs.config) cudaSupport;
+  cudaLibPath = pkgs.lib.makeLibraryPath (
+    with pkgs.cudaPackages; [
+      (lib.getLib libcublas) # libcublas.so
+      (lib.getLib cuda_cupti) # libcupti.so
+      (lib.getLib cuda_cudart) # libcudart.so
+      (lib.getLib cudnn) # libcudnn.so
+      (lib.getLib libcufft) # libcufft.so
+      (lib.getLib libcusolver) # libcusolver.so
+      (lib.getLib libcusparse) # libcusparse.so
+      (lib.getLib nccl) # libnccl.so
+      (lib.getLib libnvjitlink) # libnvJitLink.so
+      (lib.getLib pkgs.addDriverRunpath.driverLink) # libcuda.so
+    ]
+  );
 in
 {
   bbfmm3d = pkgs.callPackage ./bbfmm3d { };
@@ -16,6 +30,23 @@ in
       if cudaSupport
       then prev.jax.overridePythonAttrs { doCheck = false; }
       else prev.jax;
+    # https://github.com/NixOS/nixpkgs/pull/375186
+    jax-cuda12-pjrt = prev.jax-cuda12-pjrt.overridePythonAttrs {
+      preInstallCheck = ''
+        patchelf --add-rpath "${cudaLibPath}" $out/${final.python.sitePackages}/jax_plugins/xla_cuda12/xla_cuda_plugin.so
+      '';
+    };
+    jax-cuda12-plugin = prev.jax-cuda12-plugin.overridePythonAttrs {
+      postInstall = ''
+        mkdir -p $out/${final.python.sitePackages}/jax_cuda12_plugin/cuda/bin
+        ln -s ${pkgs.lib.getExe' pkgs.cudaPackages.cuda_nvcc "ptxas"} $out/${final.python.sitePackages}/jax_cuda12_plugin/cuda/bin
+        ln -s ${pkgs.lib.getExe' pkgs.cudaPackages.cuda_nvcc "nvlink"} $out/${final.python.sitePackages}/jax_cuda12_plugin/cuda/bin
+      '';
+      preInstallCheck = ''
+        patchelf --add-rpath "${cudaLibPath}" $out/${final.python.sitePackages}/jax_cuda12_plugin/*.so
+      '';
+      doCheck = true;
+    };
     jax-triton = final.callPackage ./jax-triton { };
     # not actually changing any dependencies, only in tests
     keras = prev.keras.overridePythonAttrs { doCheck = false; };
