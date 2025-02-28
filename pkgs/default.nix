3616ed92b87d2f0451b0b06fee5f664c228adc49
Stephen Huan <stephen.huan@cgdct.moe>
2025-02-27 23:43:48 -0500

    feat(pkgs): preliminary cudaSupport

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,8 @@
 { pkgs }:
 
+let
+  inherit (pkgs.config) cudaSupport;
+in
 {
   bbfmm3d = pkgs.callPackage ./bbfmm3d { };
   hlibpro = pkgs.callPackage ./hlibpro { };
@@ -8,10 +11,30 @@
     cola-ml = final.callPackage ./cola-ml { };
     cola-plum-dispatch = final.callPackage ./cola-plum-dispatch { };
     gpjax = final.callPackage ./gpjax { };
+    # has a tendancy to oom, even with a small number of cores
+    jax =
+      if cudaSupport
+      then prev.jax.overridePythonAttrs { doCheck = false; }
+      else prev.jax;
     jax-triton = final.callPackage ./jax-triton { };
+    # not actually changing any dependencies, only in tests
+    keras = prev.keras.overridePythonAttrs { doCheck = false; };
     kernels = final.callPackage ./kernels { };
     mugrade = final.callPackage ./mugrade { };
+    # long build, only in tests
+    onnxruntime = prev.onnxruntime.override {
+      onnxruntime = pkgs.onnxruntime.override { cudaSupport = false; };
+    };
     pbbfmm3d = final.callPackage ./pbbfmm3d { };
-    triton-cpu = final.callPackage ./triton-cpu { };
+    # use torch-bin to avoid expensive build of magma etc. on cuda
+    # and torch-no-triton to avoid recompiling when triton changes
+    torch =
+      if cudaSupport
+      # default triton is triton-bin, causing a conflict
+      then final.torch-bin.override { inherit (final) triton; }
+      else prev.torch.override { tritonSupport = false; };
+    # TODO: cudaSupport for triton-cpu
+    triton = if cudaSupport then prev.triton else final.triton-cpu;
+    triton-cpu = final.callPackage ./triton-cpu { inherit (prev) triton; };
   });
 }
