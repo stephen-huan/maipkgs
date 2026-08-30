bddd0c60d88c058b2fe26534613ebb99010f3dc4
Stephen Huan <stephen.huan@cgdct.moe>
2026-08-30 18:09:27 -0400

    gpjax: 0.13.6 -> 0.18.0

--- pkgs/gpjax/default.nix
+++ pkgs/gpjax/default.nix
@@ -1,6 +1,7 @@
 { lib
 , fetchFromGitHub
 , buildPythonPackage
+, fetchpatch2
 , hatchling
 , jax
 , optax
@@ -8,8 +9,12 @@
 , jaxtyping
 , tqdm
 , beartype
-, flax
+, equinox
+, paramax
+, lineax
+, scipy
 , numpy
+, rich
 , pytestCheckHook
 , pytest-xdist
 , mktestdocs
@@ -19,7 +24,7 @@
 
 buildPythonPackage rec {
   pname = "gpjax";
-  version = "0.13.6";
+  version = "0.18.0";
   pyproject = true;
 
   # PyPi source doesn't contain tests
@@ -27,8 +32,28 @@ buildPythonPackage rec {
     owner = "JaxGaussianProcesses";
     repo = "GPJax";
     tag = "v${version}";
-    hash = "sha256-BQAkEkyMNodPCNdPgo2rumQIOyQJtuYoTY9icqnkJaM=";
+    hash = "sha256-JaA2ZR6ctUstrGmW1OleeYSTgrJe3KslduJjnrvmjSc=";
   };
+
+  pythonRelaxDeps = [
+    "jax"
+    "jaxlib"
+  ];
+
+  patches = [
+    # https://github.com/thomaspinder/GPJax/pull/737
+    (fetchpatch2 {
+      name = "fix-mark.patch";
+      url = "https://github.com/thomaspinder/GPJax/commit/38bbe486eaeff6ca26a6457158a77e7f5b4a1307.patch?full_index=1";
+      hash = "sha256-j3lANHTi+R8WgPXl0k4kKX3ptMAHl5JTYKZ1pp924Dc=";
+    })
+    # https://github.com/thomaspinder/GPJax/pull/777
+    (fetchpatch2 {
+      name = "fix-regex.patch";
+      url = "https://github.com/thomaspinder/GPJax/commit/57bb33f986aa3564462272b0dab1d97e7cabca26.patch?full_index=1";
+      hash = "sha256-1IotFRlJp5fs/4E44kXCPetVSL5CbdBU9r3mdj+SG54=";
+    })
+  ];
 
   build-system = [
     hatchling
@@ -41,8 +66,12 @@ buildPythonPackage rec {
     jaxtyping
     tqdm
     beartype
-    flax
+    equinox
+    paramax
+    lineax
+    scipy
     numpy
+    rich
   ];
 
   nativeCheckInputs = [
@@ -57,7 +86,7 @@ buildPythonPackage rec {
     "gpjax"
   ];
 
-  pytestFlagsArray = [ "." "-v" "-n auto" ];
+  pytestFlags = [ "." "-v" "-n auto" ];
 
   meta = with lib; {
     description = "Gaussian processes in JAX";
