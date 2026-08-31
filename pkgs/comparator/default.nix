7b5c174511b5195abbba427036700cab62603c35
Stephen Huan <stephen.huan@cgdct.moe>
2026-08-31 00:33:54 -0400

    comparator: 4.29.0 -> 4.30.0

--- pkgs/comparator/default.nix
+++ pkgs/comparator/default.nix
@@ -1,5 +1,6 @@
 { lib
 , fetchFromGitHub
+, fetchpatch2
 , leanPackages
 , replaceVars
 , git
@@ -7,7 +8,7 @@
 }:
 
 leanPackages.buildLakePackage (finalAttrs: {
-  version = "4.29.0";
+  version = "4.30.0";
   pname = "comparator";
   leanPackageName = "Comparator";
 
@@ -18,16 +19,22 @@ leanPackages.buildLakePackage (finalAttrs: {
     owner = "leanprover";
     repo = "comparator";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Im6qHUxcH8Bu1Uppaex+a7vNiP5NI65p90er6dlNNSE=";
+    hash = "sha256-oOo/FUUwcVErRzhXSDNinulxNy1ywWBXA8HnDSURzPg=";
   };
 
   patches = [
     (replaceVars ./0001-build-Main.lean-hardcode-git.patch {
       inherit git;
     })
+    # https://github.com/leanprover/comparator/pull/55
+    (fetchpatch2 {
+      name = "remove-lean4checker.patch";
+      url = "https://github.com/leanprover/comparator/commit/3c972ca2d4e3fb5f336bd2c153e3bd744913b714.patch?full_index=1";
+      hash = "sha256-LqxtKbjtAK+OTpKBCp3Qo+tXrDm4k/GTQ3TuM5Cp0eE=";
+    })
   ];
 
-  leanDeps = with leanPackages; [ lean4checker lean4export ];
+  leanDeps = with leanPackages; [ lean4export ];
 
   doCheck = true;
 
